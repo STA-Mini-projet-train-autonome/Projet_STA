ce42a23b1f2da9ed7ee7cdd639c934982a3d4ef2
Unknown <leguillou.eve@gmail.com>
2018-11-06 10:28:43 +0100

    Ajout création et fermeture deThread
    Ajout dans les deux script C du code permettant de créer et fermer des Threads, selon le code à disposition sur Moodle.

--- rbc.c
+++ rbc.c
@@ -1,11 +1,162 @@
 //RBC premier commit
-
-
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>         
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
 
-int main()
+
+
+#define PORT_ECOUTE_SERVEUR     8000
+#define MAXCAR      80
+
+#define CHECKERROR(code,msg)    if (code==-1) { \
+                                    perror(msg);\
+                                    exit(1);\
+                                    }
+
+#define maxclients      10
+
+ 
+ 
+ //Definition des variables globales
+char buf_reception[MAXCAR];
+char buf_emission[MAXCAR]; 
+int flag_emission=0;
+int flag_reception=0; 
+int flag_fin=0;
+int sd; //Ma socket de dialogue
+ 
+
+//utilisation :
+//serveur.exe <port_ecoute_serveur> <adresse_ip_serevur> 
+                                    
+int main(int argc, char * argv[])
 {
-    printf("Hello world!\n");
-    return 0;
+//Section declaration des variables    
+    
+int se; //Ma socket d'ecoute
+
+struct sockaddr_in adrserv, adrclient;
+
+int sizeaddr=0;
+
+pthread_t threadReception[maxclients]; //id thread pour la reception de donnees
+pthread_t threadEmission[maxclients]; //id thread pour l'emission de donnees
+pthread_t threadTraitement[maxclients]; //id thread pour le traitement des donnees
+
+int ret; //Gestion des erreurs de creation de threads
+
+typedef struct client {
+int numclient[maxclients]; //numero du client TCP/IP
+int nbclients; //nombre de clients 
+} Tclient;
+
+Tclient clients;
+
+FILE * flog;
+
+//Section traitement
+
+// Phase 1 : Ouverture de la socket d'ecoute
+printf("\n Demarrage d'un Serveur INTERNET EN MODE CONNECTE !!! \n\n");
+se=socket(AF_INET, SOCK_STREAM, 0);//Creation de la socket d'ecoute
+CHECKERROR(se," \n\n Probleme d'ouverture de socket !!! \n\n"); //Verification que la socket a ete cree sans probleme
+
+//Definir l'adresse de la machine
+
+adrserv.sin_family=AF_INET;
+switch (argc)
+{
+    case 3:     //Utilisation : serveur_stream_thread.exe <port_ecoute_serveur> <adresse_ip_sereveur> 
+                inet_aton(argv[2],&adrserv.sin_addr);
+                //Et le numero de port
+                adrserv.sin_port=htons(atoi(argv[1]));
+                break;
+    case 2:    //Utilisation : serveur.exe <port_ecoute_serveur> 
+                adrserv.sin_addr.s_addr=INADDR_ANY;
+                //Et le numero de port
+                adrserv.sin_port=htons(atoi(argv[1]));
+                break;
+    case 1:    //Utilisation : serveur.exe
+                adrserv.sin_addr.s_addr=INADDR_ANY;
+                //Et le numero de port
+                adrserv.sin_port=htons(PORT_ECOUTE_SERVEUR);
+                break;
+    default : printf("\nLa syntaxe d'utlisation est 'serveur_stream_thread.exe <port_ecoute_serveur> <adresse_ip_sereveur> ' !!! \n");
 }
+
+// On affecte une adresse a la socket d'ecoute du serveur      
+CHECKERROR(bind(se, (const struct sockaddr *)&adrserv,sizeof(adrserv)),"\n\nProbleme de BIND !!!\n\n") ;
+
+
+
+//PHASE 2 : Attente des connexions des clients
+
+//Ouverture d'un fichier de logs
+
+flog=fopen("serveur_stream_thread.log","a+");
+if (!flog) {
+    printf("Erreur d'ouverture du fichier de logs !!! \n\n");
+    exit(-1);
+            }
+    
+//Si je suis ici c'est que j'ai affecte une adresse a ma socket d'ecoute
+//Et j'ai ouvert le fichier de logs
+
+//Initialisation des varaibles de memorisation des clients
+clients.nbclients=-1;
+
+for(int i=0; i < maxclients ; i++) clients.numclient[i]=-1; 
+
+
+while (1)
+{
+//On se met a l'ecoute des demandes d'ouverture de connexion
+listen(se,5);
+
+// On accepte une demande d'ouverture de connexion. Dans cette version on ne recupere pas les coordonnees de l'appelant
+sizeaddr=sizeof(adrclient);
+sd=accept(se, (struct sockaddr *)&adrclient, (socklen_t*) &sizeaddr);
+
+clients.nbclients++; //On incremente le nombre de client
+clients.numclient[clients.nbclients]=clients.nbclients; //On defint le client actuel
+
+printf("\n Connexion sur le serveur  du client %s avec le port %d, le numero %d et la socket %d !!! \n\n", inet_ntoa(adrclient.sin_addr), ntohs(adrclient.sin_port),clients.numclient[clients.nbclients],sd);
+fprintf(flog,"\n Connexion sur le serveur  du client %s avec le port %d,le numero %d et la socket %d !!! \n\n", inet_ntoa(adrclient.sin_addr), ntohs(adrclient.sin_port),clients.numclient[clients.nbclients],sd);
+
+//Creation des threads
+CHECKERROR(pthread_create(&threadReception[clients.numclient[clients.nbclients]], NULL, reception, NULL), "Erreur de creation du thread de Reception !!!\n");
+
+CHECKERROR(pthread_create(&threadEmission[clients.numclient[clients.nbclients]], NULL, emission, NULL), "Erreur de creation du thread d'Emission !!!\n");
+
+CHECKERROR(pthread_create(&threadTraitement[clients.numclient[clients.nbclients]], NULL, traitement, NULL), "Erreur de creation du thread d'Traitement !!! \n");
+
+/*
+while (1)
+{
+    usleep(0);
+} */
+//Synchronisation des threads a la terminaison
+
+CHECKERROR(pthread_join(threadReception[clients.numclient[clients.nbclients]], NULL), "Erreur terminaison du thread Reception !!!\n");
+CHECKERROR(pthread_join(threadEmission[clients.numclient[clients.nbclients]], NULL), "Erreur terminaison du thread Emission !!!\n");
+CHECKERROR(pthread_join(threadTraitement[clients.numclient[clients.nbclients]], NULL), "Erreur terminaison du thread Traitement !!!\n");
+
+
+}
+
+printf("Arret du programme principal !!!\n");
+
+//Fermeture des sockets
+close(sd);
+close(se);
+//Fermeture fichier
+fclose(flog);
+return 1;
+}
+            
--- trains.c
+++ trains.c
@@ -1,10 +1,131 @@
 //Trains premier commit
-
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>         
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
 
-int main()
+
+#define LOCAL_ADDR_IP     "172.31.65.163"
+#define REMOTE_ADDR_IP     "172.31.65.163"
+#define REMOTE_PORT         8000
+
+#define MAXCAR      80
+
+#define CHECKERROR(code,msg)    if (code==-1) { \
+                                    perror(msg);\
+                                    exit(1);\
+                                    }
+ 
+ 
+ //Definition des variables globales
+char buf_reception[MAXCAR];
+char buf_emission[MAXCAR]; 
+int flag_emission=0; // A 1 indique que des data sont presentes pour etre emises
+int flag_reception=0;  // A 1 indique que des data ont ete recues
+int flag_fin=0; // A 1 indique l'arret du programme
+int sd; //Ma socket de dialogue
+ 
+//utilisation :
+//client_stream_thread.exe <adresse_ip_serveur> <port_ecoute_serveur>
+                                    
+
+int main(int argc, char * argv[])
 {
-    printf("Hello world!\n");
-    return 0;
+//Declaration des variables locales
+struct sockaddr_in adrclient, adrserv;
+
+//int carlu ; //Nombre d'octets recu
+//int caremis; //Nombre d'octets emis
+
+int sizeaddr; // Taille en octets de l'adresse
+
+FILE * clientlog; //Fichier pour archiver les evenements
+
+int threadReception; //identifiant du thread de reception
+int threadEmission; //identifiant du thread de emission
+int threadTraitement; //identifiant du thread de traitement
+
+// Partie traitement 
+printf("Demarage d'un CLIENT !!! \n");
+
+//Creation de la socket
+sd=socket(AF_INET, SOCK_STREAM, 0);
+CHECKERROR(sd, "Probleme d'ouverture de socket \n");
+
+//Specification partielle de l'adresse du client - la machine locale
+/*
+adrclient.sin_family=AF_INET;
+adrclient.sin_port=htons(0); */
+
+
+//Definir l'adresse du serveur eloigne
+
+adrserv.sin_family=AF_INET;
+
+switch (argc)  
+{
+    case 3 :  //cas 1 : Utilisation : client_stream_thread.exe  <adresse_ip_serveur> <port_ecoute_serveur>
+                inet_aton(argv[1],&adrserv.sin_addr);
+                //Et le numero de port
+                adrserv.sin_port=htons(atoi(argv[2]));
+                break; 
+    case 2 :  //cas 2 : Utilisation : client_stream_thread.exe  <adresse_ip_serveur> 
+                inet_aton(argv[1],&adrserv.sin_addr);
+                //Et le numero de port defini localement
+                adrserv.sin_port=htons(REMOTE_PORT);
+                break; 
+    case 1 :  //cas 3 : Utilisation : client_stream_thread.exe 
+                inet_aton(REMOTE_ADDR_IP,&adrserv.sin_addr);
+                //Et le numero de port
+                adrserv.sin_port=htons(REMOTE_PORT);
+                break;             
+    default : printf("\n Cas imprevu !!! Gros bogue en ligne de commande !!!");
+}
+ 
+//Demande de connextion au serveur 
+CHECKERROR(connect(sd,&adrserv, sizeof(adrserv)), "\n Echec connexion !!!\n\n");
+
+// Ici cela signifie que la connexion a ete ouverte
+// Ouverture du fichier de log des data du client
+clientlog=fopen("client_stream_thread.log", "a+");
+if (!clientlog) {
+    printf("Echec d'ouverture du fichier de log du client !!!");
+    exit(-1);
+    }
+    
+    
+printf("\n Connexion sur le serveur %s avec le port %d et la socket %d !!! \n\n", inet_ntoa(adrserv.sin_addr), ntohs(adrserv.sin_port),sd);
+fprintf(clientlog,"\n Connexion sur le serveur  du client %s avec le port %d et la socket %d !!! \n\n", inet_ntoa(adrserv.sin_addr), ntohs(adrserv.sin_port),sd);
+
+//Creation des threads
+CHECKERROR(pthread_create(&threadReception, NULL, reception, NULL), "Erreur de creation du thread de Reception !!!\n");
+
+CHECKERROR(pthread_create(&threadEmission, NULL, emission, NULL), "Erreur de creation du thread d'Emission !!!\n");
+
+CHECKERROR(pthread_create(&threadTraitement, NULL, traitement, NULL), "Erreur de creation du thread d'Traitement !!! \n");
+
+
+while (!flag_fin)
+{
+    usleep(50000); // Le delai est specifie en ms. Donc cela equivant à 50s
+} 
+
+//Synchronisation des threads a la terminaison
+
+CHECKERROR(pthread_join(threadReception, NULL), "Erreur terminaison du thread Reception !!!\n");
+CHECKERROR(pthread_join(threadEmission, NULL), "Erreur terminaison du thread Emission !!!\n");
+CHECKERROR(pthread_join(threadTraitement, NULL), "Erreur terminaison du thread Traitement !!!\n");
+
+printf("\n\n Fin du dialogue cote client !!! Appuyez sur une touche ...\n\n");
+
+getchar();
+
+fclose(clientlog);
+
+close(sd);
 }
