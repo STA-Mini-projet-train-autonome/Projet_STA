81e30b90358da0eea2526d72646bc2f608887c4c
Nathan Boyer <nathan.boyer97@gmail.com>
2018-11-12 14:01:28 +0100

    Version sans erreur
    Il ne reste que des warnings !!

--- rbc.c
+++ rbc.c
@@ -126,7 +126,7 @@ int main(int argc, char * argv[]){
     for(int i=0; i < maxtrains ; i++) arguments.trains.numtrain[i]=-1;
 
     while (1){
-        CHECKERROR(pthread_create(&arguments.threads.threadCreationFils, NULL, creationFils, Targument *arguments), "Erreur de creation du thread de création de fils!!!\n");
+        CHECKERROR(pthread_create(&arguments.threads.threadCreationFils, NULL, creationFils, &arguments), "Erreur de creation du thread de création de fils!!!\n");
  
 
         // TRAITEMENT A ECRIRE ICI
@@ -153,17 +153,17 @@ int main(int argc, char * argv[]){
 void * creationFils(void * arg){
     //On se met a l'ecoute des demandes d'ouverture de connexion
     Targument *argumentsActuels = arg;
-    listen(argumentsActuels.se,5);
+    listen(argumentsActuels->se,5);
 
     // On accepte une demande d'ouverture de connexion. Dans cette version on ne recupere pas les coordonnees de l'appelant
-    sizeaddr=sizeof(argumentsActuels.adrtrain);
-    sd=accept(se, (struct sockaddr *)&argumentsActuels.adrtrain, (socklen_t*) &argumentsActuels.sizeaddr);
+    argumentsActuels->sizeaddr=sizeof(argumentsActuels->adrtrain);
+    argumentsActuels->sd=accept(argumentsActuels->se, (struct sockaddr *)&argumentsActuels->adrtrain, (socklen_t*) &argumentsActuels->sizeaddr);
 
-    argumentsActuels->trains.nbtrains = argumentsActuels.trains.nbtrains+1; //On incremente le nombre de train
-    argumentsActuels->trains.numtrain[argumentsActuels.trains.nbtrains] = argumentsActuels.trains.nbtrains; //On defint le train actuel
+    argumentsActuels->trains.nbtrains = argumentsActuels->trains.nbtrains+1; //On incremente le nombre de train
+    argumentsActuels->trains.numtrain[argumentsActuels->trains.nbtrains] = argumentsActuels->trains.nbtrains; //On defint le train actuel
 
-    printf("\n Connexion sur le serveur  du train %s avec le port %d, le numero %d et la socket %d !!! \n\n", inet_ntoa(argumentsActuels.adrtrain.sin_addr), ntohs(argumentsActuels.adrtrain.sin_port),argumentsActuels.trains.numtrain[argumentsActuels.trains.nbtrains],argumentsActuels.sd);
-    fprintf(flog,"\n Connexion sur le serveur  du train %s avec le port %d,le numero %d et la socket %d !!! \n\n", inet_ntoa(argumentsActuels.adrtrain.sin_addr), ntohs(argumentsActuels.adrtrain.sin_port),argumentsActuels.trains.numtrain[argumentsActuels.trains.nbtrains],argumentsActuels.sd);
+    printf("\n Connexion sur le serveur  du train %s avec le port %d, le numero %d et la socket %d !!! \n\n", inet_ntoa(argumentsActuels->adrtrain.sin_addr), ntohs(argumentsActuels->adrtrain.sin_port),argumentsActuels->trains.numtrain[argumentsActuels->trains.nbtrains],argumentsActuels->sd);
+    fprintf(flog,"\n Connexion sur le serveur  du train %s avec le port %d,le numero %d et la socket %d !!! \n\n", inet_ntoa(argumentsActuels->adrtrain.sin_addr), ntohs(argumentsActuels->adrtrain.sin_port),argumentsActuels->trains.numtrain[argumentsActuels->trains.nbtrains],argumentsActuels->sd);
     // On crée le tunnel 
     int descripteur[2];
     tunnel = pipe(descripteur); // Il faut que cette ligne soit imperativement placee avant le fork
@@ -175,16 +175,16 @@ void * creationFils(void * arg){
     pid=fork();
     if (pid!=0) { //Je suis dans le code du pere
         printf("\n Je suis le pere du processus de pid : %d \n\n", pid);
-        CHECKERROR(pthread_create(&argumentsActuels.threads.threadReceptionDonneesFils, NULL, receptionDonneesFils, NULL), "Erreur de creation du thread de réception des données du fils !!! \n");
-        CHECKERROR(pthread_create(&argumentsActuels.threads.threadReceptionDemandeConnexion, NULL, receptionDemandeConnexion, NULL), "Erreur de creation du thread de réception d'une demande de connexion !!! \n");
-        close(argumentsActuels.sd); // Le pere ferme la socket de dialogue pour ne pas interferer avec le fils
+        CHECKERROR(pthread_create(&argumentsActuels->threads.threadReceptionDonneesFils, NULL, receptionDonneesFils, NULL), "Erreur de creation du thread de réception des données du fils !!! \n");
+        CHECKERROR(pthread_create(&argumentsActuels->threads.threadReceptionDemandeConnexion, NULL, receptionDemandeConnexion, NULL), "Erreur de creation du thread de réception d'une demande de connexion !!! \n");
+        close(argumentsActuels->sd); // Le pere ferme la socket de dialogue pour ne pas interferer avec le fils
     }
 
     else { //Je suis dans le code du fils
 
     //Creation des threads
-    CHECKERROR(pthread_create(&argumentsActuels.threads.threadReceptionDonneesFilsTrainPrecedent[argumentsActuels.trains.numtrain[argumentsActuels.trains.nbtrains]], NULL, receptionDonneesFilsTrainPrecedent, NULL), "Erreur de creation du thread de réception des données du train précédent!!!\n");
-    CHECKERROR(pthread_create(&argumentsActuels.threads.threadReceptionDonneesTrain[argumentsActuels.trains.numtrain[argumentsActuels.trains.nbtrains]], NULL, receptionDonneesTrain, NULL), "Erreur de creation du thread de réception des données du train !!!\n");
+    CHECKERROR(pthread_create(&argumentsActuels->threads.threadReceptionDonneesFilsTrainPrecedent[argumentsActuels->trains.numtrain[argumentsActuels->trains.nbtrains]], NULL, receptionDonneesFilsTrainPrecedent, NULL), "Erreur de creation du thread de réception des données du train précédent!!!\n");
+    CHECKERROR(pthread_create(&argumentsActuels->threads.threadReceptionDonneesTrain[argumentsActuels->trains.numtrain[argumentsActuels->trains.nbtrains]], NULL, receptionDonneesTrain, NULL), "Erreur de creation du thread de réception des données du train !!!\n");
     }
 }
 
