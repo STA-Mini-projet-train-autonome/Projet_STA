c0ff02d02d8e31b8e1375fd374695d945bca882f
Gescilam <gescilam@gmail.com>
2018-11-13 11:30:12 +0100

    Mise à jour du RBC
    On a ajouter de commentaire, et on a traité d'autres cas, et on a mise en forme. Mais, il manque encore les fonctions.

--- rbc.c
+++ rbc.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>         
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -18,15 +18,14 @@
                                     exit(1);\
                                     }
 
-#define maxtrains      10
+#define maxtrains      10 //Pour le moment
 
- 
- 
+
 //Definition des variables globales
 char buf_reception[MAXCAR];
-char buf_emission[MAXCAR]; 
+char buf_emission[MAXCAR];
 int flag_emission=0;
-int flag_reception=0; 
+int flag_reception=0;
 int flag_fin=0;
 
 
@@ -41,18 +40,18 @@ void * envoiDonneesTrain();
 
 void * creationFils();
 
-FILE * flog;
+FILE * flog; //fichiers log sont comme un «journal»
 
-struct sockaddr_in adrserv, adrtrain; 
+struct sockaddr_in adrserv, adrtrain;
 
 typedef struct {
-    int pid;
-    int numtrain;
+    int pid;        //numero du PID du fils pour ce train là
+    int numtrain;   //ID du train
 } couple;
 
 typedef struct train {
     couple ensTrain[maxtrains]; //A CHANGER PAR UNE LISTE SIMPLEMENT CHAINEE
-    int nbtrains; //nombre de trains 
+    int nbtrains; //nombre de trains
     } Ttrain;
 
 //Creation d'une structure qui regroupe toutes les threads qui seront utilisees
@@ -63,12 +62,15 @@ typedef struct thread {
     pthread_t threadReceptionDonneesFilsTrainPrecedent[maxtrains];
     pthread_t threadReceptionDonneesTrain[maxtrains];
 } Tthread;
-int pid;
+
+int pid; //declaration du numero du PID du fils pour le train
 
 int tunnel[2*maxtrains][2];
 
+/*Creation d'une structure qui sera passee en argument des fonctions
+servant a la creation des threads, et qui contient tous les parametres
+necessaires a l'execution de ces fonctions*/
 
-//Creation d'une structure qui sera passee en argument des fonctions servant a la creation des threads, et qui contient tous les parametres necessaires a l'execution de ces fonctions
 typedef struct argument {
     int sizeaddr;
     struct sockaddr_in adrtrain;
@@ -80,91 +82,88 @@ typedef struct argument {
 
 int main(int argc, char * argv[]){
     //Section declaration des variables
-    
+
     Targument arguments;
     arguments.sizeaddr=0;
-    int ret; //Gestion des erreurs de creation de threads
+    int ret; //Gestion des erreurs de creation des threads
 
+    //Section traitement
 
-//Section traitement
+    // Phase 1 : Ouverture de la socket d'ecoute
+    printf("\n Demarrage d'un Serveur INTERNET EN MODE CONNECTE !!! \n\n");
+    arguments.se=socket(AF_INET, SOCK_STREAM, 0);//Creation de la socket d'ecoute
+    CHECKERROR(arguments.se," \n\n Probleme d'ouverture de socket !!! \n\n"); //Verification que la socket a ete cree sans probleme
 
-// Phase 1 : Ouverture de la socket d'ecoute
-printf("\n Demarrage d'un Serveur INTERNET EN MODE CONNECTE !!! \n\n");
-arguments.se=socket(AF_INET, SOCK_STREAM, 0);//Creation de la socket d'ecoute
-CHECKERROR(arguments.se," \n\n Probleme d'ouverture de socket !!! \n\n"); //Verification que la socket a ete cree sans probleme
+    //Definir l'adresse de la machine
 
-//Definir l'adresse de la machine
+    adrserv.sin_family=AF_INET;
+    switch (argc){
+        case 3:     //Utilisation : serveur_stream_thread.exe <port_ecoute_serveur> <adresse_ip_sereveur>
+                    inet_aton(argv[2],&adrserv.sin_addr);
+                    //Et le numero de port
+                    adrserv.sin_port=htons(atoi(argv[1]));
+                    break;
+        case 2:    //Utilisation : serveur.exe <port_ecoute_serveur>
+                    adrserv.sin_addr.s_addr=INADDR_ANY;
+                    //Et le numero de port
+                    adrserv.sin_port=htons(atoi(argv[1]));
+                    break;
+        case 1:    //Utilisation : serveur.exe
+                    adrserv.sin_addr.s_addr=INADDR_ANY;
+                    //Et le numero de port
+                    adrserv.sin_port=htons(PORT_ECOUTE_SERVEUR);
+                    break;
+        default : printf("\nLa syntaxe d'utlisation est 'serveur_stream_thread.exe <port_ecoute_serveur> <adresse_ip_sereveur> ' !!! \n");
+    }
 
-adrserv.sin_family=AF_INET;
-switch (argc)
-{
-    case 3:     //Utilisation : serveur_stream_thread.exe <port_ecoute_serveur> <adresse_ip_sereveur> 
-                inet_aton(argv[2],&adrserv.sin_addr);
-                //Et le numero de port
-                adrserv.sin_port=htons(atoi(argv[1]));
-                break;
-    case 2:    //Utilisation : serveur.exe <port_ecoute_serveur> 
-                adrserv.sin_addr.s_addr=INADDR_ANY;
-                //Et le numero de port
-                adrserv.sin_port=htons(atoi(argv[1]));
-                break;
-    case 1:    //Utilisation : serveur.exe
-                adrserv.sin_addr.s_addr=INADDR_ANY;
-                //Et le numero de port
-                adrserv.sin_port=htons(PORT_ECOUTE_SERVEUR);
-                break;
-    default : printf("\nLa syntaxe d'utlisation est 'serveur_stream_thread.exe <port_ecoute_serveur> <adresse_ip_sereveur> ' !!! \n");
-}
+    // On affecte une adresse a la socket d'ecoute du serveur
+    CHECKERROR(bind(arguments.se, (const struct sockaddr *)&adrserv,sizeof(adrserv)),"\n\nProbleme de BIND !!!\n\n");
+    //atribuition de l'adresse du socket d'écoute
 
-// On affecte une adresse a la socket d'ecoute du serveur      
-CHECKERROR(bind(arguments.se, (const struct sockaddr *)&adrserv,sizeof(adrserv)),"\n\nProbleme de BIND !!!\n\n") ;
+    //PHASE 2 : Attente des connexions des trains
 
-//PHASE 2 : Attente des connexions des trains
+    //Ouverture d'un fichier de logs
 
-//Ouverture d'un fichier de logs
+    flog = fopen("serveur_stream_thread.log","a+");
 
-flog=fopen("serveur_stream_thread.log","a+");
-if (!flog) {
-    printf("Erreur d'ouverture du fichier de logs !!! \n\n");
-    exit(-1);
-            }
-    
+    if (!flog) {
+        printf("Erreur d'ouverture du fichier de logs !!! \n\n");
+        exit(-1);
+    }
+
     //Si je suis ici c'est que j'ai affecte une adresse a ma socket d'ecoute
     //Et j'ai ouvert le fichier de logs
 
     //Initialisation des variables de memorisation des trains
     arguments.trains.nbtrains=-1;
 
-for(int i=0; i < maxtrains ; i++) {
-    arguments.trains.ensTrain[i].pid=-1; 
-    arguments.trains.ensTrain[i].numtrain=-1;
-}
+    for(int i=0; i < maxtrains ; i++) {
+        arguments.trains.ensTrain[i].pid=-1;
+        arguments.trains.ensTrain[i].numtrain=-1;
+    }
 
-while (1)
-{
-   
-
-    CHECKERROR(pthread_create(&arguments.threads.threadCreationFils, NULL, creationFils, &arguments), "Erreur de creation du thread de création de fils!!!\n");
- 
+    while (1){
+        CHECKERROR(pthread_create(&arguments.threads.threadCreationFils, NULL, creationFils, &arguments), "Erreur de creation du thread de création de fils!!!\n");
 
         // TRAITEMENT A ECRIRE ICI
 
         //Synchronisation des threads a la terminaison
-    CHECKERROR(pthread_join(arguments.threads.threadCreationFils, NULL), "Erreur terminaison du thread de réception d'une demande de connexion !!!\n");
-    CHECKERROR(pthread_join(arguments.threads.threadReceptionDonneesFilsTrainPrecedent[arguments.trains.ensTrain[arguments.trains.nbtrains].numtrain], NULL), "Erreur terminaison du thread réception des données du train précédent !!!\n");
-    CHECKERROR(pthread_join(arguments.threads.threadReceptionDonneesTrain[arguments.trains.ensTrain[arguments.trains.nbtrains].numtrain], NULL), "Erreur terminaison du thread de réception des données du train !!!\n");
-    CHECKERROR(pthread_join(arguments.threads.threadReceptionDonneesFils, NULL), "Erreur terminaison du thread de réception des données du fils !!!\n");
-    CHECKERROR(pthread_join(arguments.threads.threadReceptionDemandeConnexion, NULL), "Erreur terminaison du thread de réception d'une demande de connexion !!!\n");
-    }
+        CHECKERROR(pthread_join(arguments.threads.threadCreationFils, NULL), "Erreur terminaison du thread de réception d'une demande de connexion !!!\n");
+        CHECKERROR(pthread_join(arguments.threads.threadReceptionDonneesFilsTrainPrecedent[arguments.trains.ensTrain[arguments.trains.nbtrains].numtrain], NULL), "Erreur terminaison du thread réception des données du train précédent !!!\n");
+        CHECKERROR(pthread_join(arguments.threads.threadReceptionDonneesTrain[arguments.trains.ensTrain[arguments.trains.nbtrains].numtrain], NULL), "Erreur terminaison du thread de réception des données du train !!!\n");
+        CHECKERROR(pthread_join(arguments.threads.threadReceptionDonneesFils, NULL), "Erreur terminaison du thread de réception des données du fils !!!\n");
+        CHECKERROR(pthread_join(arguments.threads.threadReceptionDemandeConnexion, NULL), "Erreur terminaison du thread de réception d'une demande de connexion !!!\n");
+        }
 
-    printf("Arret du programme principal !!!\n");
+        printf("Arret du programme principal !!!\n");
 
-    //Fermeture des sockets
-    close(arguments.sd);
-    close(arguments.se);
-    
-    //Fermeture fichier
-    fclose(flog);
+        //Fermeture des sockets
+        close(arguments.sd);
+        close(arguments.se);
+
+        //Fermeture fichier
+        fclose(flog);
+
     return 1;
 }
 
@@ -181,88 +180,117 @@ void * creationFils(void * arg){
     argumentsActuels->trains.ensTrain[argumentsActuels->trains.nbtrains].numtrain = argumentsActuels->trains.nbtrains; //On defint le train actuel
     printf("\n Connexion sur le serveur  du train %s avec le port %d, le numero %d et la socket %d !!! \n\n", inet_ntoa(argumentsActuels->adrtrain.sin_addr), ntohs(argumentsActuels->adrtrain.sin_port),argumentsActuels->trains.ensTrain[argumentsActuels->trains.nbtrains].numtrain,argumentsActuels->sd);
     fprintf(flog,"\n Connexion sur le serveur  du train %s avec le port %d,le numero %d et la socket %d !!! \n\n", inet_ntoa(argumentsActuels->adrtrain.sin_addr), ntohs(argumentsActuels->adrtrain.sin_port),argumentsActuels->trains.ensTrain[argumentsActuels->trains.nbtrains].numtrain,argumentsActuels->sd);
-    // On crée le tunnel 
+
+    // On crée le tunnel en faisant leur division entre «tunnel du fils» jusqu'à maxtrains et «tunnel du père» dès maxtrains jusqu'à 2*maxtrains
     int tunnelPere;
     int tunnelFils;
-    tunnelFils = pipe(tunnel[argumentsActuels->trains.ensTrain[argumentsActuels->trains.nbtrains].numtrain]); // Il faut que cette ligne soit impérativement placée avant le fork
+
+    // Il faut que cette ligne soit impérativement placée avant le fork
+    tunnelFils = pipe(tunnel[argumentsActuels->trains.ensTrain[argumentsActuels->trains.nbtrains].numtrain]);
     tunnelPere = pipe(tunnel[argumentsActuels->trains.ensTrain[argumentsActuels->trains.nbtrains].numtrain+maxtrains]);
-    if (tunnelFils != 0|| tunnelPere != 0)
+    //de cette maniere le fils et le pere partagent la meme zone memoire
+
+    if (tunnelFils != 0 || tunnelPere != 0)
         printf("Problème avec la création des tunnels");
-    /*  Pour écrire dans le tunnel, on utilise : ssize_t write(int entreeTube, const void *elementAEcrire, size_t nombreOctetsAEcrire); 
+
+    /*  Pour écrire dans le tunnel, on utilise : ssize_t write(int entreeTube, const void *elementAEcrire, size_t nombreOctetsAEcrire);
     Pour lire le tunnel, on utilise : ssize_t read(int sortieTube, void *elementALire, size_t nombreOctetsALire);*/
-    pid=fork();
-    if (pid!=0) { //Je suis dans le code du pere
+
+    pid = fork(); //numero du processus qui identifie chaque fils
+    //pid < 0: échec de la création du fils
+    //pid == 0: cotê du fils
+    //pid > 0: cotê du père, c'est aussi l'ID du processus du fils
+
+    if (pid > 0) { //Je suis dans le code du pere
         printf("\n Je suis le pere du processus de pid : %d \n\n", pid);
+
+        //Ferme la partie du tunnel du fils
         close(tunnel[argumentsActuels->trains.ensTrain[argumentsActuels->trains.nbtrains].numtrain][0]);
         close(tunnel[argumentsActuels->trains.ensTrain[argumentsActuels->trains.nbtrains].numtrain+maxtrains][1]);
+
+        //Creation des threads
         CHECKERROR(pthread_create(&argumentsActuels->threads.threadReceptionDonneesFils, NULL, receptionDonneesFils, argumentsActuels), "Erreur de creation du thread de réception des données du fils !!! \n");
         CHECKERROR(pthread_create(&argumentsActuels->threads.threadReceptionDemandeConnexion, NULL, receptionDemandeConnexion, argumentsActuels), "Erreur de creation du thread de réception d'une demande de connexion !!! \n");
-        
+
         close(argumentsActuels->sd); // Le pere ferme la socket de dialogue pour ne pas interfere avec le fils
     }
+    else if (pid == 0){ //Je suis dans le code du fils
+        argumentsActuels->trains.ensTrain[argumentsActuels->trains.nbtrains].pid = pid; //On récupère le pid du train
 
-    else { //Je suis dans le code du fils
-    argumentsActuels->trains.ensTrain[argumentsActuels->trains.nbtrains].pid=pid; //On récupère le pid du train
+        //Ferme la partie du tunnel du père
+        close(tunnel[argumentsActuels->trains.ensTrain[argumentsActuels->trains.nbtrains].numtrain][1]);
+        close(tunnel[argumentsActuels->trains.ensTrain[argumentsActuels->trains.nbtrains].numtrain+maxtrains][0]);
 
-    close(tunnel[argumentsActuels->trains.ensTrain[argumentsActuels->trains.nbtrains].numtrain][1]);
-    close(tunnel[argumentsActuels->trains.ensTrain[argumentsActuels->trains.nbtrains].numtrain+maxtrains][0]);
-    //Creation des threads
-    CHECKERROR(pthread_create(&argumentsActuels->threads.threadReceptionDonneesFilsTrainPrecedent[argumentsActuels->trains.ensTrain[argumentsActuels->trains.nbtrains].numtrain], NULL, receptionDonneesFilsTrainPrecedent, argumentsActuels), "Erreur de creation du thread de réception des données du train précédent!!!\n");
-    CHECKERROR(pthread_create(&argumentsActuels->threads.threadReceptionDonneesTrain[argumentsActuels->trains.ensTrain[argumentsActuels->trains.nbtrains].numtrain], NULL, receptionDonneesTrain, argumentsActuels), "Erreur de creation du thread de réception des données du train !!!\n");
+        //Creation des threads
+        CHECKERROR(pthread_create(&argumentsActuels->threads.threadReceptionDonneesFilsTrainPrecedent[argumentsActuels->trains.ensTrain[argumentsActuels->trains.nbtrains].numtrain], NULL, receptionDonneesFilsTrainPrecedent, argumentsActuels), "Erreur de creation du thread de réception des données du train précédent!!!\n");
+        CHECKERROR(pthread_create(&argumentsActuels->threads.threadReceptionDonneesTrain[argumentsActuels->trains.ensTrain[argumentsActuels->trains.nbtrains].numtrain], NULL, receptionDonneesTrain, argumentsActuels), "Erreur de creation du thread de réception des données du train !!!\n");
+
+        close(argumentsActuels->se); // Les fils ferme la socket d'écoute pour ne pas interfere avec le père
     }
+
+    else { //Échec de la création du fils
+        printf("\n Échec de la création du fils !! \n");
+        }
 }
 
 void * receptionDonneesFils( void * arg){
     Targument *argumentsActuels = arg;
     char messageEcrire[MAXCAR];
     char messageLire[MAXCAR];
-    if (pid!=0) { //Je suis dans le code du pere
+
+    if (pid > 0) { //Je suis dans le code du pere
         sprintf(messageEcrire, "Bonjour, fils. Je suis ton père !");
         printf("Nous sommes dans le père (pid = %d).\nIl envoie le message suivant au fils : \"%s\".\n\n\n", getpid(), messageEcrire);
         write(tunnel[argumentsActuels->trains.ensTrain[argumentsActuels->trains.nbtrains].numtrain+maxtrains][1], messageEcrire, MAXCAR);
     }
-    else {
+    else if (pid == 0) { //Je suis dans le cote du fils
         read(tunnel[argumentsActuels->trains.ensTrain[argumentsActuels->trains.nbtrains].numtrain+maxtrains][0], messageLire, MAXCAR);
-        printf("Nous sommes dans le fils (pid = %d).\nIl a reçu le message suivant du père : \"%s\".\n\n\n", getpid(), messageLire); 
+        printf("Nous sommes dans le fils (pid = %d).\nIl a reçu le message suivant du père : \"%s\".\n\n\n", getpid(), messageLire);
     }
-}   
+    else { //Échec de la création du fils
+        printf("\n Échec de la création du fils dans la recepition des données du fils !! \n");
+        }
+}
 
 //Fonction servant a etablir pour la premiere fois une connexion entre un train et le RBC
 void * receptionDemandeConnexion( void * arg){
     Targument *argumentsActuels = arg;
     int carlus;
+
     do{
-        printf("Réception en cours de la socket %d !!! \n",argumentsActuels->sd);
+        printf("Réception en cours de la socket %d !!! \n", argumentsActuels->sd);
         getchar();
-        carlus = read(argumentsActuels->sd,buf_reception,MAXCAR);
+        carlus = read(argumentsActuels->sd,buf_reception, MAXCAR);
+
         if (!carlus) printf("Aucun caractère reçu !!! \n");
         else{
-            flag_reception=1;
-            printf("Réception effectuée de %d caractères !!! \n",carlus);
+            flag_reception = 1;
+            printf("Réception effectuée de %d caractères !!! \n", carlus);
             // Ajout du numéro de train dans le tableau
         }
     }
     while (!flag_fin);
+
     printf("Fin du thread de réception de la demande de connexion !!! \n");
-    pthread_exit(0);
-} 
+    pthread_exit(0); //ferme le thread de demande de connexion
+}
 
 void * receptionDonneesFilsTrainPrecedent( void * arg){
-    
-} 
+//Eve
+}
 
 void * receptionDonneesTrain( void * arg){
-
-} 
+//Marcela
+}
 
 void * envoiDonneesFilsDuTrainSuivant( void * arg){
-    
-} 
+//Eve
+}
 
 void * envoiDonneesRBC( void * arg){
-    
-} 
+//Eve
+}
 
 void * envoiDonneesTrain( void * arg){
-    
-} 
+//Marcela
+}
