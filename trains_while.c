75725ac281880763a52c7c46a0ef1a708b27209a
Nathan Boyer <nathan.boyer97@gmail.com>
2018-11-15 19:59:56 +0100

    Ajout du code du train de tête
    Version correctement indentée qui marche (normalement)

--- trains_while.c
+++ trains_while.c
@@ -115,7 +115,12 @@ int main(int argc, char * argv[]){
 
     int sizeaddr; // Taille en octets de l'adresse
     
-  //  FILE * clientlog; //Fichier pour archiver les evenements
+    char * pch;
+    int ordre;
+    double vitesse;
+    double position;
+    
+    //  FILE * clientlog; //Fichier pour archiver les evenements
 
     //Creation de la socket
     sd=socket(AF_INET, SOCK_STREAM, 0);
@@ -168,17 +173,75 @@ int main(int argc, char * argv[]){
     
    // fprintf(clientlog,"\n Connexion sur le serveur du client %s avec le port %d et la socket %d !!! \n\n", inet_ntoa(adrserv.sin_addr), ntohs(adrserv.sin_port),sd);
     
+    pch = strtok (buf_emission,";"); //On sépare la chaîne de caractère reçu selon le caractère ;
+    ordre = atoi(&pch[0]);
+    printf("Ordre : %d\n", ordre);
     
     int carlus;
-    while(1){
-        //Emission
-        //printf("Position du train : \n");
-        //gets(buf_emission);
-        //
+    int t=1;
+    
+    if (ordre == 0){ //Je suis dans le train tete
+        pch = strtok (NULL, ";");
+        position = atof(pch);
+        printf("PositionEspace : %f\n",position);
+        
+        pch = strtok (NULL, ";");
+        vitesse = atof(pch);
+        printf("Vitesse : %f\n", vitesse);
+        
+        while(t<30){
+            
+            if (t<10){
+                position += vitesse/t;
+                vitesse++;
+                sprintf(buf_emission, "%lf;%lf", position, vitesse);
+                caremis = write(sd, buf_emission, strlen(buf_emission)+1);
+                //buf_emission = NULL;
+                printf("Vitesse increment!: '%lf' \n position: '%lf' \n", vitesse, position);
+                carlus=read(sd,buf_reception, MAXCAR);
+                printf("Donnees reçues : %s \n", buf_reception);
+                //do sprintf(buf_emission,getchar());
+                //while (buf_emission!='\n' && buf_emission!=EOF);
+                usleep(1000000);
+            }
+            else if (t>=10 && t < 20){
+                position += vitesse/t;
+                sprintf(buf_emission, "%lf;%lf", position, vitesse);
+                caremis = write(sd, buf_emission, strlen(buf_emission)+1);
+                printf("Vitesse constant! '%lf' \n position: '%lf' \n", vitesse, position);
+                carlus=read(sd,buf_reception, MAXCAR);
+                printf("Donnees reçues : %s \n", buf_reception);
+                //do sprintf(buf_emission,getchar());
+                //while (buf_emission!='\n' && buf_emission!=EOF);
+                //fflush(buf_emission);
+                usleep(1000000);
+            }
+            else{
+                position += vitesse/t;
+                vitesse--;
+                sprintf(buf_emission, "%lf;%lf", position, vitesse);
+                caremis = write(sd, buf_emission, strlen(buf_emission)+1);
+                printf("Vitesse decrement!: '%lf' \n position: '%lf' \n", vitesse, position);
+                carlus=read(sd,buf_reception, MAXCAR);
+                printf("Donnees reçues : %s \n", buf_reception);
+                //do sprintf(buf_emission,getchar());
+                //while (buf_emission!='\n' && buf_emission!=EOF);
+                //fflush(buf_emission);
+                usleep(1000000);
+            }
+            t++;
+        }
+    }
+    else{
+        while(1){
+            //Emission
+            //printf("Position du train : \n");
+            //gets(buf_emission);
+            //
 //=================== RECUPERATION DE LA POSITION PAR BALISE GPS ====================        
-        do {
-            rdlen = read(fd, buf, sizeof(buf) - 1);
-            if (rdlen > 0) {
+            do {
+                rdlen = read(fd, buf, sizeof(buf) - 1);
+                if (rdlen > 0) {
                     unsigned char *p;
                     //printf("Read %d:", rdlen);
                     unsigned char byte;
@@ -191,18 +254,18 @@ int main(int argc, char * argv[]){
                         //printf("t :  %x \n", trame_hexa[trameReception]);
                         trameReception++;
                         if (trameReception==29){
-                           // printf("=======Fin de la Trame=======\n");
+                            // printf("=======Fin de la Trame=======\n");
                             trameReception=0;
                             verif=0;
                             trameRecue=1;
                         }
                     }
                     else {
-                    /* Rechecherche du debut de la trame*/
+                        /* Rechecherche du debut de la trame*/
                         switch (verif){
                             case 0:
                                 if (byte == 0xff){
-                                   // printf("=====Debut de trame possible=====\n");
+                                    // printf("=====Debut de trame possible=====\n");
                                     verif++;
                                     trame_hexa[trameReception]=byte;
                                     trameReception++;
@@ -273,56 +336,55 @@ int main(int argc, char * argv[]){
                 }
                 //on vérifie que la balise est bien celle qu'on souhaite observer
                 if (trameRecue && (trame_hexa[22]!=NUM_BALISE_GPS)){ 
-		    printf("trame recue mais mauvaise balise");
+                    printf("trame recue mais mauvaise balise");
                     trameRecue=0;
                 }
-        } while (!trameRecue);
-        printf("Reception d'une trame complete : \n");
-        int i;
-        for (i=0;i<29; i++){
-            printf ("%x ",trame_hexa[i]);
-        }
-        printf("\n");
-        trameRecue=0;
-        adressOfHedgehog=trame_hexa[22];
-        timestamp=trame_hexa[5]+(trame_hexa[6]<<8)+(trame_hexa[7]<<16)+(trame_hexa[8]<<24);
-        cor_x=trame_hexa[9]+(trame_hexa[10]<<8)+(trame_hexa[11]<<16)+(trame_hexa[12]<<24);
-        cor_y=trame_hexa[13]+(trame_hexa[14]<<8)+(trame_hexa[15]<<16)+(trame_hexa[16]<<24);
-        printf("\n Le train portant la balise %hhu a pour coordonnees x=%li et y=%li a t=%lu ms.\n\n",adressOfHedgehog,cor_x,cor_y,timestamp);
+            } while (!trameRecue);
+            printf("Reception d'une trame complete : \n");
+            int i;
+            for (i=0;i<29; i++){
+                printf ("%x ",trame_hexa[i]);
+            }
+            printf("\n");
+            trameRecue=0;
+            adressOfHedgehog=trame_hexa[22];
+            timestamp=trame_hexa[5]+(trame_hexa[6]<<8)+(trame_hexa[7]<<16)+(trame_hexa[8]<<24);
+            cor_x=trame_hexa[9]+(trame_hexa[10]<<8)+(trame_hexa[11]<<16)+(trame_hexa[12]<<24);
+            cor_y=trame_hexa[13]+(trame_hexa[14]<<8)+(trame_hexa[15]<<16)+(trame_hexa[16]<<24);
+            printf("\n Le train portant la balise %hhu a pour coordonnees x=%li et y=%li a t=%lu ms.\n\n",adressOfHedgehog,cor_x,cor_y,timestamp);
 
 //============== FIN DE LA RECUPERATION DE LA POSITION PAR BALISE GPS ===============
 
-        printf("Envoi position et vitesse\n");
-        //pour l'instant on considère qu'on se déplace uniquement selon l'ase des x donc on utilise la coordonnée en x
-        long position = cor_x; //en mm
-        double vitesse =(double)(position-positionPrecedent) / ((double) (timestamp - tpsPrecedent));
-	tpsPrecedent = timestamp;
-	positionPrecedent=position;
-        sprintf(buf_emission,"%lf;%lf",(double)position,vitesse);
-        caremis=write(sd,buf_emission, strlen(buf_emission)+1);
+            printf("Envoi position et vitesse\n");
+            //pour l'instant on considère qu'on se déplace uniquement selon l'ase des x donc on utilise la coordonnée en x
+            long position = cor_x; //en mm
+            double vitesse =(double)(position-positionPrecedent) / ((double) (timestamp - tpsPrecedent));
+            tpsPrecedent = timestamp;
+            positionPrecedent=position;
+            sprintf(buf_emission,"%lf;%lf",(double)position,vitesse);
+            caremis=write(sd,buf_emission, strlen(buf_emission)+1);
             if (!caremis) printf("Aucun caractère émis !!!\n");
             //else printf("%d caractères émis !!!\n",caremis);
-        //Reception
-    
-        //printf("Réception en cours sur la socket %d !!! \n", sd);
-        carlus=read(sd,buf_reception, MAXCAR);
-        printf("Donnees reçues : %s \n", buf_reception);
-        //printf("Essai1\n");
-        if (!carlus) printf("Aucun caractère reçu !!! \n");
-        // Mise à jour de la simulation avec les nouvelles valeurs
-        else{
-            char * pch;
-            double positionTrainDevant, vitesseTrainDevant;
-            pch = strtok (buf_reception,";"); //On sépare la chaîne de caractère reçu selon le caractère ;
-            positionTrainDevant = atof(&pch[0]);
-            printf("Position du train devant : %lf\n",positionTrainDevant);
-            pch = strtok(NULL, ";");
-            vitesseTrainDevant = atof(pch);
-            printf("Vitesse du train devant : %lf\n",vitesseTrainDevant);
+            
+            //Reception
+            //printf("Réception en cours sur la socket %d !!! \n", sd);
+            carlus=read(sd,buf_reception, MAXCAR);
+            printf("Donnees reçues : %s \n", buf_reception);
+            //printf("Essai1\n");
+            if (!carlus) printf("Aucun caractère reçu !!! \n");
+            // Mise à jour de la simulation avec les nouvelles valeurs
+            else{
+                char * pch;
+                double positionTrainDevant, vitesseTrainDevant;
+                pch = strtok (buf_reception,";"); //On sépare la chaîne de caractère reçu selon le caractère ;
+                positionTrainDevant = atof(&pch[0]);
+                printf("Position du train devant : %lf\n",positionTrainDevant);
+                pch = strtok(NULL, ";");
+                vitesseTrainDevant = atof(pch);
+                printf("Vitesse du train devant : %lf\n",vitesseTrainDevant);
+            }
         }
     }
-    //Synchronisation des threads a la terminaison
-
 
     printf("\n\n Fin du dialogue cote client !!! Appuyez sur une touche ...\n\n");
 
