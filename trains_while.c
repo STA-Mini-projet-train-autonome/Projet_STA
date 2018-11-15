d109e2dfbc215f2fd6e43d8d86417762d5f064c9
Marcela <marcela_modolo@hotmail.com>
2018-11-15 17:32:37 +0100

    Train tête

--- trains_while.c
+++ trains_while.c
@@ -13,8 +13,8 @@
 #include <termios.h>
 
 
-#define LOCAL_ADDR_IP     "172.30.133.37"
-#define REMOTE_ADDR_IP     "172.30.131.57"
+#define LOCAL_ADDR_IP     "127.0.0.1"
+#define REMOTE_ADDR_IP     "192.168.0.195"
 #define REMOTE_PORT         8000
 #define MAXCAR      80
 
@@ -35,7 +35,7 @@ int flag_reception=0;  // A 1 indique que des data ont ete recues
 int flag_fin=0; // A 1 indique l'arret du programme
 //Ma socket de dialogue
 
-unsigned long tpsPrecedent;
+unsigned long tpsPrecedent=0;
 
 
 //Parametrage du port serie
@@ -114,6 +114,11 @@ int main(int argc, char * argv[]){
     //int caremis; //Nombre d'octets emis
 
     int sizeaddr; // Taille en octets de l'adresse
+
+    char * pch; 
+    int ordre;
+    double vitesse;
+    double position;
     
   //  FILE * clientlog; //Fichier pour archiver les evenements
 
@@ -150,11 +155,12 @@ int main(int argc, char * argv[]){
     printf("\n Connexion sur le serveur %s avec le port %d et la socket %d !!! \n\n", inet_ntoa(adrserv.sin_addr), ntohs(adrserv.sin_port),sd);
     
     int caremis; //Nombre d'octets emis
+    
     printf("Entrez l'ordre du train, sa position initiale ainsi que sa vitesse initiale sous le format 'ordre;position;vitesse' : ");
     gets(buf_emission);
     caremis = write(sd, buf_emission, strlen(buf_emission)+1);
     if (caremis) {
-        printf("\n Message émis !!!  \n");
+        printf("\n Message émis !!! \n");
     }
     else printf("\n Il y a un problème !!!\n");
 
@@ -167,158 +173,219 @@ int main(int argc, char * argv[]){
     }*/
     
    // fprintf(clientlog,"\n Connexion sur le serveur du client %s avec le port %d et la socket %d !!! \n\n", inet_ntoa(adrserv.sin_addr), ntohs(adrserv.sin_port),sd);
-    
-    
+
+    pch = strtok (buf_emission,";"); //On sépare la chaîne de caractère reçu selon le caractère ;      
+    ordre = atoi(&pch[0]);
+    printf("Ordre : %d\n", ordre);
+
     int carlus;
-    while(1){
-        //Emission
-        //printf("Position du train : \n");
-        //gets(buf_emission);
-        //
-//=================== RECUPERATION DE LA POSITION PAR BALISE GPS ====================        
-        do {
-            rdlen = read(fd, buf, sizeof(buf) - 1);
-            if (rdlen > 0) {
-                    unsigned char *p;
-                    //printf("Read %d:", rdlen);
-                    unsigned char byte;
-                    for (p = buf; rdlen-- > 0; p++){
-                        byte=*p;
-                      //  printf(" %x \n ", byte);
-                    }
-                    if(trameReception>4){
-                        trame_hexa[trameReception]=byte;
-                        //printf("t :  %x \n", trame_hexa[trameReception]);
-                        trameReception++;
-                        if (trameReception==29){
-                           // printf("=======Fin de la Trame=======\n");
-                            trameReception=0;
-                            verif=0;
-                            trameRecue=1;
-                        }
-                    }
-                    else {
-                    /* Rechecherche du debut de la trame*/
-                        switch (verif){
-                            case 0:
-                                if (byte == 0xff){
-                                   // printf("=====Debut de trame possible=====\n");
-                                    verif++;
-                                    trame_hexa[trameReception]=byte;
-                                    trameReception++;
-                                    //printf("verif : %d\n",verif);
-                                }
-                                break;
-                            case 1:
-                                if (byte == 0x47){
-                                    verif++;
-                                    trame_hexa[trameReception]=byte;
-                                    trameReception++;
-                                    //printf("verif : %d\n",verif);
-                                }
-                                else {
-                                    verif=0;
-                                    //printf("Erreur de detection\n");
-                                    trameReception=0;
-                                }
-                                break;
-                            case 2:
-                                if (byte == 0x11){
-                                    verif++;
-                                    trame_hexa[trameReception]=byte;
-                                    trameReception++;
-                                    //printf("verif : %d\n",verif);
-                                }
-                                else {
-                                    verif=0;
-                                    trameReception=0;
-                                    //printf("Erreur de detection\n");
-                                }
-                                break;
-                            case 3:
-                                if (byte == 0x00){
-                                    verif++;
-                                    trame_hexa[trameReception]=byte;
-                                    trameReception++;
-                                    //printf("verif : %d\n",verif);
-                                }
-                                else {
-                                    verif=0;
-                                    trameReception=0;
-                                    //printf("Erreur de detection\n");
-                                }
-                                break;
-                            case 4:
-                                if (byte == 0x16){
-                                    verif++;
-                                    trame_hexa[trameReception]=byte;
-                                    trameReception++;
-                                    //printf("verif : %d\n",verif);
-                                }
-                                else {
-                                    trameReception=0;
-                                    verif=0;
-                                    //printf("Erreur de detection \n");
-                                }
-                                break;
-                            default:
-                                //printf("Erreur sur verif, reinitialisation\n");
-                                trameReception=0;
-                                verif=0;
-                        }
-                    }
-                }
-                else if (rdlen < 0) {
-                            printf("Error from read: %d: %s\n", rdlen, strerror(errno));
-                }
-                //on vérifie que la balise est bien celle qu'on souhaite observer
-                if (trameRecue && (trame_hexa[22]!=NUM_BALISE_GPS)){ 
-		    printf("trame recue mais mauvaise balise");
-                    trameRecue=0;
-                }
-        } while (!trameRecue);
-        printf("Reception d'une trame complete : \n");
-        int i;
-        for (i=0;i<29; i++){
-            printf ("%x ",trame_hexa[i]);
-        }
-        printf("\n");
-        trameRecue=0;
-        adressOfHedgehog=trame_hexa[22];
-        timestamp=trame_hexa[5]+(trame_hexa[6]<<8)+(trame_hexa[7]<<16)+(trame_hexa[8]<<24);
-        cor_x=trame_hexa[9]+(trame_hexa[10]<<8)+(trame_hexa[11]<<16)+(trame_hexa[12]<<24);
-        cor_y=trame_hexa[13]+(trame_hexa[14]<<8)+(trame_hexa[15]<<16)+(trame_hexa[16]<<24);
-        printf("\n Le train portant la balise %hhu a pour coordonnees x=%li et y=%li a t=%lu ms.\n\n",adressOfHedgehog,cor_x,cor_y,timestamp);
+    int t=1;
 
-//============== FIN DE LA RECUPERATION DE LA POSITION PAR BALISE GPS ===============
+    if (ordre == 0){ //Je suis dans le train tete
+        pch = strtok (NULL, ";");
+        position = atof(pch);
+        printf("PositionEspace : %f\n",position);
 
-        printf("Envoi position et vitesse\n");
-        //pour l'instant on considère qu'on se déplace uniquement selon l'ase des x donc on utilise la coordonnée en x
-        double position = cor_x; //en mm
-        double vitesse = 13.987;
-        sprintf(buf_emission,"%lf;%lf",position,vitesse);
-        caremis=write(sd,buf_emission, strlen(buf_emission)+1);
-            if (!caremis) printf("Aucun caractère émis !!!\n");
-            //else printf("%d caractères émis !!!\n",caremis);
-        //Reception
-    
-        //printf("Réception en cours sur la socket %d !!! \n", sd);
-        carlus=read(sd,buf_reception, MAXCAR);
-        printf("Donnees reçues : %s \n", buf_reception);
-        //printf("Essai1\n");
-        if (!carlus) printf("Aucun caractère reçu !!! \n");
-        // Mise à jour de la simulation avec les nouvelles valeurs
-        else{
-            char * pch;
-            double positionTrainDevant, vitesseTrainDevant;
-            pch = strtok (buf_reception,";"); //On sépare la chaîne de caractère reçu selon le caractère ;
-            positionTrainDevant = atof(&pch[0]);
-            printf("Position du train devant : %lf\n",positionTrainDevant);
-            pch = strtok(NULL, ";");
-            vitesseTrainDevant = atof(pch);
-            printf("Vitesse du train devant : %lf\n",vitesseTrainDevant);
+        pch = strtok (NULL, ";");
+        vitesse = atof(pch);
+        printf("Vitesse : %f\n", vitesse);
+
+        while(t<30){
+
+            if (t<10){
+                position += vitesse/t;
+                vitesse++;            
+                sprintf(buf_emission, "%lf;%lf", position, vitesse);
+                caremis = write(sd, buf_emission, strlen(buf_emission)+1);
+                //buf_emission = NULL;
+                printf("Vitesse increment!: '%lf' \n position: '%lf' \n", vitesse, position);
+                carlus=read(sd,buf_reception, MAXCAR);
+                printf("Donnees reçues : %s \n", buf_reception);
+                //do sprintf(buf_emission,getchar()); 
+                //while (buf_emission!='\n' && buf_emission!=EOF);
+                usleep(1000000);
+            }
+            else if (t>=10 && t < 20){
+                position += vitesse/t;
+                sprintf(buf_emission, "%lf;%lf", position, vitesse);
+                caremis = write(sd, buf_emission, strlen(buf_emission)+1);
+                printf("Vitesse constant! '%lf' \n position: '%lf' \n", vitesse, position);
+                carlus=read(sd,buf_reception, MAXCAR);
+                printf("Donnees reçues : %s \n", buf_reception);
+                //do sprintf(buf_emission,getchar());  
+                //while (buf_emission!='\n' && buf_emission!=EOF);
+                //fflush(buf_emission);
+                usleep(1000000);
+            }
+            else{
+                position += vitesse/t;
+                vitesse--;
+                sprintf(buf_emission, "%lf;%lf", position, vitesse);
+                caremis = write(sd, buf_emission, strlen(buf_emission)+1);
+                printf("Vitesse decrement!: '%lf' \n position: '%lf' \n", vitesse, position);
+                carlus=read(sd,buf_reception, MAXCAR);
+                printf("Donnees reçues : %s \n", buf_reception);
+                //do sprintf(buf_emission,getchar());  
+                //while (buf_emission!='\n' && buf_emission!=EOF);
+                //fflush(buf_emission);
+                usleep(1000000);
+            }
+            t++;
         }
     }
+    else{
+        while(1){
+            //Emission
+            //printf("Position du train : \n");
+            //gets(buf_emission);
+            //
+            }
+    //=================== RECUPERATION DE LA POSITION PAR BALISE GPS ====================        
+            do {
+                rdlen = read(fd, buf, sizeof(buf) - 1);
+                if (rdlen > 0) {
+                        unsigned char *p;
+                        //printf("Read %d:", rdlen);
+                        unsigned char byte;
+                        for (p = buf; rdlen-- > 0; p++){
+                            byte=*p;
+                          //  printf(" %x \n ", byte);
+                        }
+                        if(trameReception>4){
+                            trame_hexa[trameReception]=byte;
+                            //printf("t :  %x \n", trame_hexa[trameReception]);
+                            trameReception++;
+                            if (trameReception==29){
+                               // printf("=======Fin de la Trame=======\n");
+                                trameReception=0;
+                                verif=0;
+                                trameRecue=1;
+                            }
+                        }
+                        else {
+                        /* Rechecherche du debut de la trame*/
+                            switch (verif){
+                                case 0:
+                                    if (byte == 0xff){
+                                       // printf("=====Debut de trame possible=====\n");
+                                        verif++;
+                                        trame_hexa[trameReception]=byte;
+                                        trameReception++;
+                                        //printf("verif : %d\n",verif);
+                                    }
+                                    break;
+                                case 1:
+                                    if (byte == 0x47){
+                                        verif++;
+                                        trame_hexa[trameReception]=byte;
+                                        trameReception++;
+                                        //printf("verif : %d\n",verif);
+                                    }
+                                    else {
+                                        verif=0;
+                                        //printf("Erreur de detection\n");
+                                        trameReception=0;
+                                    }
+                                    break;
+                                case 2:
+                                    if (byte == 0x11){
+                                        verif++;
+                                        trame_hexa[trameReception]=byte;
+                                        trameReception++;
+                                        //printf("verif : %d\n",verif);
+                                    }
+                                    else {
+                                        verif=0;
+                                        trameReception=0;
+                                        //printf("Erreur de detection\n");
+                                    }
+                                    break;
+                                case 3:
+                                    if (byte == 0x00){
+                                        verif++;
+                                        trame_hexa[trameReception]=byte;
+                                        trameReception++;
+                                        //printf("verif : %d\n",verif);
+                                    }
+                                    else {
+                                        verif=0;
+                                        trameReception=0;
+                                        //printf("Erreur de detection\n");
+                                    }
+                                    break;
+                                case 4:
+                                    if (byte == 0x16){
+                                        verif++;
+                                        trame_hexa[trameReception]=byte;
+                                        trameReception++;
+                                        //printf("verif : %d\n",verif);
+                                    }
+                                    else {
+                                        trameReception=0;
+                                        verif=0;
+                                        //printf("Erreur de detection \n");
+                                    }
+                                    break;
+                                default:
+                                    //printf("Erreur sur verif, reinitialisation\n");
+                                    trameReception=0;
+                                    verif=0;
+                            }
+                        }
+                    }
+                    else if (rdlen < 0) {
+                                printf("Error from read: %d: %s\n", rdlen, strerror(errno));
+                    }
+                    //on vérifie que la balise est bien celle qu'on souhaite observer
+                    if (trameRecue && (trame_hexa[22]!=NUM_BALISE_GPS)){ 
+    		    printf("trame recue mais mauvaise balise");
+                        trameRecue=0;
+                    }
+            } while (!trameRecue);
+            printf("Reception d'une trame complete : \n");
+            int i;
+            for (i=0;i<29; i++){
+                printf ("%x ",trame_hexa[i]);
+            }
+            printf("\n");
+            trameRecue=0;
+            adressOfHedgehog=trame_hexa[22];
+            timestamp=trame_hexa[5]+(trame_hexa[6]<<8)+(trame_hexa[7]<<16)+(trame_hexa[8]<<24);
+            cor_x=trame_hexa[9]+(trame_hexa[10]<<8)+(trame_hexa[11]<<16)+(trame_hexa[12]<<24);
+            cor_y=trame_hexa[13]+(trame_hexa[14]<<8)+(trame_hexa[15]<<16)+(trame_hexa[16]<<24);
+            printf("\n Le train portant la balise %hhu a pour coordonnees x=%li et y=%li a t=%lu ms.\n\n",adressOfHedgehog,cor_x,cor_y,timestamp);
+
+    //============== FIN DE LA RECUPERATION DE LA POSITION PAR BALISE GPS ===============
+
+            printf("Envoi position et vitesse\n");
+            //pour l'instant on considère qu'on se déplace uniquement selon l'ase des x donc on utilise la coordonnée en x
+            double position = cor_x; //en mm
+            double vitesse = cor_x / ((double) timestamp-tpsPrecedent);
+            tpsPrecedent = timestamp;
+
+            sprintf(buf_emission,"%lf;%lf",position,vitesse);
+            caremis=write(sd,buf_emission, strlen(buf_emission)+1);
+                if (!caremis) printf("Aucun caractère émis !!!\n");
+                //else printf("%d caractères émis !!!\n",caremis);
+            //Reception
+        
+            //printf("Réception en cours sur la socket %d !!! \n", sd);
+            carlus=read(sd,buf_reception, MAXCAR);
+            printf("Donnees reçues : %s \n", buf_reception);
+            //printf("Essai1\n");
+            if (!carlus) printf("Aucun caractère reçu !!! \n");
+            // Mise à jour de la simulation avec les nouvelles valeurs
+            else{
+                char * pch;
+                double positionTrainDevant, vitesseTrainDevant;
+                pch = strtok (buf_reception,";"); //On sépare la chaîne de caractère reçu selon le caractère ;
+                positionTrainDevant = atof(&pch[0]);
+                printf("Position du train devant : %lf\n",positionTrainDevant);
+                pch = strtok(NULL, ";");
+                vitesseTrainDevant = atof(pch);
+                printf("Vitesse du train devant : %lf\n",vitesseTrainDevant);
+            }
+        }
     //Synchronisation des threads a la terminaison
 
 
@@ -330,4 +397,3 @@ int main(int argc, char * argv[]){
 
     close(sd);
 }
-
